eba60e9e25f9aefa73db75fa3bbbb746e0e82145
kiszner.laszlo <kiszner.laszlo@gmail.com>
2024-02-20 18:22:51 +0100

    basic authentication

---
+++ src/main/java/com/home/address/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.home.address.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1")
+                .password(passwordEncoder().encode("user1Pass"))
+                .authorities("ROLE_USER");
+    }
+
+}
